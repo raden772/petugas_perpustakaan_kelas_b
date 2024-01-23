2dd5f3562fbfe486c4a2358f17d2e54a93c423c5
delandapp <alihandronikolas@gmail.com>
2024-01-23 14:52:43 +0700

    first commit

---
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:dio/dio.dart' as dio;
+import '../../../data/constant/endpoint.dart';
+import '../../../data/provider/api_provider.dart';
+import '../../../data/provider/storage_provider.dart';
+import '../../../routes/app_pages.dart';
+
+class LoginController extends GetxController {
+  final loading = false.obs;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  final count = 0.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+  void increment() => count.value++;
+
+  login() async {
+    loading(true);
+    try {
+      FocusScope.of(Get.context!).unfocus(); // nge close keyboard
+      formKey.currentState?.save();
+      if (formKey.currentState!.validate()) {
+        final response = await ApiProvider.instance().post(Endpoint.login,
+            data: dio.FormData.fromMap({
+              "username": usernameController.text.toString(),
+              "password": passwordController.text.toString()
+            }));
+        if (response.statusCode == 200) {
+          await StorageProvider.write(StorageKey.status, "Logged");
+          Get.offAllNamed(Routes.HOME);
+        } else {
+          Get.snackbar("Sorry", "Login Gagal", backgroundColor: Colors.orange);
+        }
+      }
+      loading(false);
+    } on dio.DioException catch (e) {
+      loading(false);
+      if (e.response != null) {
+        if (e.response?.data != null) {
+          Get.snackbar("Sorry", "${e.response?.data['message']}",
+              backgroundColor: Colors.orange);
+        }
+      } else {
+        Get.snackbar("Sorry", e.message ?? "", backgroundColor: Colors.red);
+      }
+    } catch (e) {
+      Get.snackbar("Error", e.toString(), backgroundColor: Colors.red);
+    }
+  }
+}
